caac308124461625eec7b2424213d7129a093a1e
Станислав Маяцкий <stanislavmayatsky@192.168.10.22>
2025-01-29 21:24:36 +0500

    Implemented form validation for forbidden words for Category and Product models

--- catalog/forms.py
+++ catalog/forms.py
@@ -2,10 +2,14 @@
 Формы для ввода данных.
 """
 
+import os
+
+from dotenv import load_dotenv
 from django import forms
 
 from catalog.models import Category, Product
 
+load_dotenv()
 
 class CategoryForm(forms.ModelForm):
     """Форма для ввода данных о категории товаров."""
@@ -21,14 +25,30 @@ class CategoryForm(forms.ModelForm):
         """Стилизация формы добавления категории."""
         super(CategoryForm, self).__init__(*args, **kwargs)
         for field in self.fields:
-            self.fields[field].widget.attrs.update(
-                {
+            self.fields[field].widget.attrs.update({
                     "class": "form-control",
                     "placeholder": self.fields[field].label,
                     "style": "font-size: 0.9em; width: 100%",
-                }
-            )
-        
+                })
+
+    def clean(self):
+        forbidden_words = os.getenv("FORBIDDEN_WORDS").split(",")
+        cleaned_data = super().clean()
+
+        cleaned_name = cleaned_data.get("name").split()
+        for word in cleaned_name:
+            if word.lower() in forbidden_words:
+                self.add_error("name", "Поле с наименованием категории содержит запрещённое слово %s." % word)
+                break
+
+        cleaned_description = cleaned_data.get("description").split()
+        for word in cleaned_description:
+            if word.lower() in forbidden_words:
+                self.add_error("description", "Поле с описанием категории содержит запрещённое слово %s." % word)
+                break
+
+        return cleaned_data
+
 
 class ProductForm(forms.ModelForm):
     """
@@ -58,3 +78,22 @@ class ProductForm(forms.ModelForm):
                     "style": "font-size: 0.9em; width: 100%",
                 }
             )
+
+
+    def clean(self):
+        forbidden_words = os.getenv("FORBIDDEN_WORDS").split(",")
+        cleaned_data = super().clean()
+
+        cleaned_product = cleaned_data.get("name").split()
+        for word in cleaned_product:
+            if word.lower() in forbidden_words:
+                self.add_error("name", "Поле с наименованием продукта содержит запрещённое слово %s." % word)
+                break
+
+        cleaned_description = cleaned_data.get("description").split()
+        for word in cleaned_description:
+            if word.lower() in forbidden_words:
+                self.add_error("description", "Поле с описанием продукта содержит запрещённое слово %s." % word)
+                break
+
+        return cleaned_data
